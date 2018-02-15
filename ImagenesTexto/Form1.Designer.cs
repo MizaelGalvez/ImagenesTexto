64d58b2ecaca1d4d2d8b393ba127fad24e260e03
@MizaelGalvez <mizaelgalvez@gmail.com>
2018-02-15 09:47:51 -0700

    Initial commit

---
+++ ImagenesTexto/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ImagenesTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(663, 271);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 89.53846F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.46154F));
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.83691F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.16309F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(325, 265);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 198);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(212, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 95.39877F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 4.601227F));
+            this.tableLayoutPanel3.Controls.Add(this.button2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(334, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.86188F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75.13812F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 9F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(326, 265);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(305, 57);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Procesar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 186);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 271);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Mizael Analisis de texto";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ImagenesTexto/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ImagenesTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+
+
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+
+        // Replace the subscriptionKey string value with your valid subscription key.
+        const string subscriptionKey = "";
+
+        // Replace or verify the region.
+        //
+        // You must use the same region in your REST API call as you used to obtain your subscription keys.
+        // For example, if you obtained your subscription keys from the westus region, replace 
+        // "westcentralus" in the URI below with "westus".
+        //
+        // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+        // a free trial subscription key, you should not need to change this region.
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/recognizeText";
+        string imagen = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"‪C:\Users\Usuario\Desktop\";
+            openFileDialog1.Filter = " JPG files (*.JPG)|*. JPG|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                        imagen = openFileDialog1.FileName;
+                        pictureBox1.BackgroundImage = Image.FromFile(imagen);
+                    
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("El archivo seleccionado no es un tipo de imagen válido");
+                }
+            }
+
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Handwriting Recognition:");
+            Console.Write("Enter the path to an image with handwritten text you wish to read: ");
+            string imageFilePath = imagen;
+
+            // Execute the REST API call.
+            ReadHandwrittenText(imageFilePath);
+
+            Console.WriteLine("\nPlease wait a moment for the results to appear. Then, press Enter to exit...\n");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the handwritten text from the specified image file by using the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file with handwritten text.</param>
+        public async void ReadHandwrittenText(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameter. Set "handwriting" to false for printed text.
+            string requestParameters = "handwriting=true";
+
+            // Assemble the URI for the REST API Call.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response = null;
+
+            // This operation requrires two REST API calls. One to submit the image for processing,
+            // the other to retrieve the text found in the image. This value stores the REST API
+            // location to call to retrieve the text.
+            string operationLocation = null;
+
+            // Request body. Posts a locally stored JPEG image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+            ByteArrayContent content = new ByteArrayContent(byteData);
+
+            // This example uses content type "application/octet-stream".
+            // You can also use "application/json" and specify an image URL.
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            // The first REST call starts the async process to analyze the written text in the image.
+            response = await client.PostAsync(uri, content);
+
+            // The response contains the URI to retrieve the result of the process.
+            if (response.IsSuccessStatusCode)
+                operationLocation = response.Headers.GetValues("Operation-Location").FirstOrDefault();
+            else
+            {
+                // Display the JSON error data.
+                textBox1.Text = "\nError: en Dimenciones de la imagen o en peso del archivo, maximo 4 megas y una resolucion no mayor a 1080x900\n";
+                Console.WriteLine("\nError:\n");
+                Console.WriteLine(JsonPrettyPrint(await response.Content.ReadAsStringAsync()));
+                return;
+            }
+
+            // The second REST call retrieves the text written in the image.
+            //
+            // Note: The response may not be immediately available. Handwriting recognition is an
+            // async operation that can take a variable amount of time depending on the length
+            // of the handwritten text. You may need to wait or retry this operation.
+            //
+            // This example checks once per second for ten seconds.
+            string contentString;
+            int i = 0;
+            do
+            {
+                System.Threading.Thread.Sleep(1000);
+                response = await client.GetAsync(operationLocation);
+                contentString = await response.Content.ReadAsStringAsync();
+                ++i;
+            }
+            while (i < 10 && contentString.IndexOf("\"status\":\"Succeeded\"") == -1);
+
+            if (i == 10 && contentString.IndexOf("\"status\":\"Succeeded\"") == -1)
+            {
+                textBox1.Text = "\nError en Comunicacion con el Servidor\n";
+                Console.WriteLine("\nTimeout error.\n");
+                return;
+            }
+
+            // Display the JSON response.
+            Console.WriteLine("\nResponse:\n");
+            Console.WriteLine(JsonPrettyPrint(contentString));
+
+
+
+
+
+
+                string json = (JsonPrettyPrint(contentString));
+
+                JObject rss = JObject.Parse(json);
+                string rssTitle = (string)rss["recognitionResult"]["text"];
+            // James Newton-King
+
+            string itemTitle1 = "";
+            string itemTitle2 = "";
+            string itemTitle3 = "";
+            string itemTitle4 = "";
+            string itemTitle5 = "";
+            string itemTitle6 = "";
+            string itemTitle7 = "";
+            string itemTitle8 = "";
+
+            try
+            {
+                 itemTitle1 = (string)rss["recognitionResult"]["lines"][0]["text"];
+            }
+            catch (Exception)
+            {
+                
+            }
+            try
+            {
+                 itemTitle2 = (string)rss["recognitionResult"]["lines"][1]["text"];
+            }
+            catch (Exception)
+            {
+                
+            }
+            try
+            {
+                 itemTitle3 = (string)rss["recognitionResult"]["lines"][2]["text"];
+            }
+            catch (Exception)
+            {
+                
+            }
+            try
+            {
+                 itemTitle4 = (string)rss["recognitionResult"]["lines"][3]["text"];
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                itemTitle5 = (string)rss["recognitionResult"]["lines"][4]["text"];
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                itemTitle6 = (string)rss["recognitionResult"]["lines"][5]["text"];
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                itemTitle7 = (string)rss["recognitionResult"]["lines"][6]["text"];
+            }
+            catch (Exception)
+            {
+
+            }
+            try
+            {
+                itemTitle8 = (string)rss["recognitionResult"]["lines"][7]["text"];
+            }
+            catch (Exception)
+            {
+
+            }
+            // Json.NET 1.3 + New license + Now on CodePlex
+            // ["Json.NET", "CodePlex"]
+
+            // Json.NET
+            // CodePlex
+            textBox1.Text = textBox1.Text + "\n" + itemTitle1.ToString() +" ";
+            textBox1.Text = textBox1.Text + "\n" + itemTitle2.ToString() + " ";
+            textBox1.Text = textBox1.Text + "\n" + itemTitle3.ToString() + " ";
+            textBox1.Text = textBox1.Text + "\n" + itemTitle4.ToString() + " ";
+            textBox1.Text = textBox1.Text + "\n" + itemTitle5.ToString() + " ";
+            textBox1.Text = textBox1.Text + "\n" + itemTitle6.ToString() + " ";
+            textBox1.Text = textBox1.Text + "\n" + itemTitle7.ToString() + " ";
+            textBox1.Text = textBox1.Text + "\n" + itemTitle8.ToString() + " ";
+        }
+        
+    /// <summary>
+    /// Returns the contents of the specified file as a byte array.
+    /// </summary>
+    /// <param name="imageFilePath">The image file to read.</param>
+    /// <returns>The byte array of the image data.</returns>
+    static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+
+        /// <summary>
+        /// Formats the given JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string to format.</param>
+        /// <returns>789e225204a046a0be0cf04e51278d5f</returns>
+        public string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            try
+                            {
+                            sb.Append(new string(' ', ++offset * indentLength));
+
+                            }
+                            catch (Exception)
+                            {
+                                
+                            }
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            try
+                            {
+                            sb.Append(new string(' ', --offset * indentLength));
+
+                            }
+                            catch (Exception)
+                            {
+                                
+                            }
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            try
+                            {
+                            sb.Append(new string(' ', offset * indentLength));
+
+                            }
+                            catch (Exception)
+                            {
+                                
+                            }
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+
+
+
+
+        }
+
+
+    }
+}
